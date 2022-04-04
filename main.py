84049826e0fc6e2a5c2eda6a2d5e8bea4e84ea8a
vens8 <vensr.maddula@gmail.com>
2022-04-04 21:41:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ Can use the user objects to login and give more personalised responses.
 
 Instead of infinitely running While True for listening to commands, limit to 2-3. So, stop when there's no
 valid command consecutively for 2-3 times (using boolean, assert). 
+
+Note: Might have to change the name of the voice assistant as 'Hexard' is not being recognised.
 '''
 
 
@@ -32,7 +34,7 @@ class User:
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[1].id)  # Female Voice
 
 
 def speak(text):
@@ -43,16 +45,16 @@ def speak(text):
 def take_command():
     try:
         with sr.Microphone() as source:
-            winsound.PlaySound("listenAlert.wav", winsound.SND_FILENAME)  # Play sound before listening
             print('listening...')
+            winsound.PlaySound("listenAlert.wav", winsound.SND_FILENAME)  # Play sound before listening
             listener.adjust_for_ambient_noise(source, 2.5)
-            listener.pause_threshold = 1
-            voice = listener.listen(source, timeout=10, phrase_time_limit=10)
+            listener.pause_threshold = 1  # Check these parameters
+            voice = listener.listen(source, timeout=2, phrase_time_limit=10)
             # voice = listener.listen(source)
             command = listener.recognize_google(voice)
             if command:
                 command = command.lower()
-                speak(command)
+                # speak(command)
                 return command
     except sr.UnknownValueError:
         return
@@ -68,14 +70,22 @@ def run_hexard():
                 song = command.replace('play', '')
                 speak('Now playing ' + song)
                 pywhatkit.playonyt(song)
+            elif 'on youtube' in command:
+                video = command.replace('on youtube', '')
+                speak('Now playing ' + video)
+                pywhatkit.playonyt(video)
             elif 'time' in command:
                 time = datetime.datetime.now().strftime('%I:%M %p')
                 speak('Current time is ' + time)
+            elif 'hello' in command:
+                speak("Hey, what's up?")
             elif 'who is' in command:
                 person = command.replace('who is', '')
                 info = wikipedia.summary(person, 1)
                 print(info)
                 speak(info)
+            elif 'your name' in command:
+                speak("Wow, I thought you knew me. Well, I'm Hexard, nice to meet you.")
             elif 'what is' in command:
                 speak("Searching Wikipedia...")
                 thing = command.replace('what is', '')
@@ -86,15 +96,28 @@ def run_hexard():
                 speak('sorry, I have a headache')
             elif 'who are you' in command:
                 speak("Wow, I thought you knew me. Well, I'm Hexard, nice to meet you.")
+            elif 'how are you' in command:
+                speak("I'll be great as long as I'm electrically powered up, hehe.")
             elif 'are you single' in command:
                 speak("Hehe, why do you ask? I am in a relationship with Siri. The male one tho.")
             elif 'joke' in command:
                 speak(pyjokes.get_joke())
+            elif 'hexard' in command:
+                speak('Yes, how may I help you?')
             elif 'bye' in command:
-                speak("Aww, sad to see you go. Catch you later!")
+                speak("Aww, sad to see you go. Catch you later!")  # Add user name
+                exit(0)  # Deliberate exit
+            elif 'open youtube' in command:
+                speak('Opening Youtube')
+                webbrowser.open("https://youtube.com")
             else:
                 speak("I didn't seem to catch that, please say your command again.")
+                speak(f"Did you really mean to say {command}")
+
+                elseCommand = True  # To take further input from user as "yes or no" if required
+
         else:
+            print("this")
             speak("I didn't seem to catch that, please say your command again.")
 
     except TypeError:
